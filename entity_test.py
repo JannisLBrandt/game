08df2b913e45947bea6ed2fb0c7bd7b6bc11fe6f
jannisBrndt <jannis.brandt@alumni.fh-aachen.de>
2025-03-20 19:15:33 +0100

    updated entity_test.py

--- entity_test.py
+++ entity_test.py
@@ -2,9 +2,22 @@ import unittest
 import entity
 
 class TestEntity(unittest.TestCase):
-    def test_name(self):
-        object = entity.Entity("test", 0)
-        self.assertEqual(object.get_name(), "test")
+    def setUp(self):
+        self.entity = entity.Entity("test", 0)
+
+    def test_get_name(self):
+        self.assertEqual(self.entity.get_name(), "test")
+
+    def test_set_name(self):
+        self.entity.set_name("test1")
+        self.assertEqual(self.entity.get_name(), "test1")
+
+    def test_get_health(self):
+        self.assertEqual(self.entity.get_health(), 0)
+
+    def test_set_health(self):
+        self.entity.set_health(1)
+        self.assertEqual(self.entity.get_health(), 1)
 
 if __name__ == '__main__':
     unittest.main()
