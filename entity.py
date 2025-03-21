b36b01e9fe31676f786306b78c6a6fc69ad3da87
jannisBrndt <jannis.brandt@alumni.fh-aachen.de>
2025-03-21 14:14:22 +0100

    updated entity.py and test_entity.py

--- entity.py
+++ entity.py
@@ -4,11 +4,13 @@ class Entity:
                  name,
                  health,
                  attack,
-                 physical_resistance):
+                 physical_resistance,
+                 magic_resistance):
         self.name = name
         self.health = health
         self.attack = attack
         self.physical_resistance = physical_resistance
+        self.magic_resistance = magic_resistance
 
     # get the name of the entity
     def get_name(self):
@@ -17,12 +19,16 @@ class Entity:
     # get thr health of the entity
     def get_health(self):
         return self.health
-
+    # get the attack of the entity
     def get_attack(self):
         return self.attack
-
+    # get the physical_resistance of the entity
     def get_physical_resistance(self):
         return self.physical_resistance
+
+    # get the magica_resistance of the entity
+    def get_magic_resistance(self):
+        return self.magic_resistance
     
     # set the name of the entity
     def set_name(self, amount):
@@ -31,9 +37,15 @@ class Entity:
     # set the health of the entity
     def set_health(self, amount):
         self.health = amount
-
+    
+    # set the attack of the entity
     def set_attack(self, amount):
         self.attack = amount
-
+    
+    # set the physical_resistance of the entity
     def set_physical_resistance(self, amount):
         self.physical_resistance = amount
+
+    # set the magic_resistance of the entity
+    def set_magic_resistance(self, amount):
+        self.magic_resistance = amount
--- test_entity.py
+++ test_entity.py
@@ -4,29 +4,33 @@ import entity
 class TestEntity(unittest.TestCase):
     # creates the same entity for every test, resets changes from previous ones
     def setUp(self):
-        self.entity = entity.Entity("test", 0, 0, 0)
-
+        self.entity = entity.Entity("test", 0, 0, 0, 0)
+    
+    # test  attribute name
     def test_get_name(self):
         self.assertEqual(self.entity.get_name(), "test")
 
     def test_set_name(self):
         self.entity.set_name("test1")
         self.assertEqual(self.entity.get_name(), "test1")
-
+    
+    # test attribute health
     def test_get_health(self):
         self.assertEqual(self.entity.get_health(), 0)
 
     def test_set_health(self):
         self.entity.set_health(1)
         self.assertEqual(self.entity.get_health(), 1)
-
+    
+    # test attribute attack
     def test_get_attack(self):
         self.assertEqual(self.entity.get_attack(), 0)
 
     def test_set_attack(self):
         self.entity.set_attack(1)
         self.assertEqual(self.entity.get_attack(), 1)
-
+    
+    # test attribute physical_resistance
     def test_get_physical_resistance(self):
         self.assertEqual(self.entity.get_physical_resistance(), 0)
 
@@ -34,6 +38,13 @@ class TestEntity(unittest.TestCase):
         self.entity.set_physical_resistance(1)
         self.assertEqual(self.entity.get_physical_resistance(), 1)
     
+    # test attribute magical_resistance
+    def test_get_magic_resistance(self):
+        self.assertEqual(self.entity.get_magic_resistance(), 0)
+
+    def test_set_magic_resistance(self):
+        self.entity.set_magic_resistance(1)
+        self.assertEqual(self.entity.get_magic_resistance(), 1)
 
 if __name__ == '__main__':
     unittest.main()
