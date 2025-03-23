911923271fc64dfabad22f13c3740520e447f815
jannisBrndt <jannis.brandt@alumni.fh-aachen.de>
2025-03-23 10:41:01 +0100

    Updated entity.py and test_entity.py

--- entity.py
+++ entity.py
@@ -1,3 +1,6 @@
+# for regular expressions
+import re
+
 # class for creating an entity in the game
 class Entity:
     def __init__(self,
@@ -49,3 +52,71 @@ class Entity:
     # set the magic_resistance of the entity
     def set_magic_resistance(self, amount):
         self.magic_resistance = amount
+
+def input_reader():
+    name = input("Enter your entities' name: ")
+    health = input("Enter your entities' health: ")
+    attack = input("Enter your entities' attack: ")
+    physical_resistance = input("Enter your entities' physical resistance: ")
+    magic_resistance = input("Enter your entities' magic resistance: ")
+    return name, health, attack, physical_resistance, magic_resistance
+
+# function to check for valid input
+def validate_input(name, health, attack, physical_resistance, magic_resistance):
+    # validate name is of type string
+    if type(name) != str:
+        print(f"Error: name attribute is of type: {type(name)}")
+        return
+    # validate health is of type float
+    if type(health) != float:
+        print(f"Error: attribute health is of type: {type(health)}")
+        return
+    # validate attack is of type float
+    if type(attack) != float:
+        print(f"Error: attribute attack is of type: {type(health)}")
+        return
+    # validate physical resistance is of type float 
+    if type(physical_resistance) != float:
+        print(f"Error: attribute physical_resistance is og type: {type(physical_resistance)}")
+        return
+    # validate magical resistance is of type float
+    if type(magic_resistance) != float:
+        print(f"Error: attribute magic resistance is of type: {type(magic_resistance)}")
+        return
+
+# function to check for number in string
+def is_numeric(a):
+    pattern = r"-?\d+(\.\d+)?"
+    return re.match(pattern, a)
+        
+def parse_input(name, health, attack, physical_resistance, magic_resistance):
+    # check attribute name
+    if not isinstance(name, str):
+        name = str(name)
+    # numerical attributes need to be checked that if the string is a number convert it else display error
+    # check attribute health
+    if is_numeric(health):
+        health = float(health)
+    else:
+        raise ValueError("Health must be a numeric value")
+    if is_numeric(attack):
+        attack = float(attack)
+    else:
+        raise ValueError("Attack must be a numeric value")
+    if is_numeric(physical_resistance):
+        physical_resistance = float(physical_resistance)
+    else:
+        raise ValueError("Physical resistance must be a numerical value")
+    if is_numeric(magic_resistance):
+        magic_resistance = float(magic_resistance)
+    else:
+        raise ValueError("Magic resistance must be a numerical value")
+    return name, health, attack, physical_resistance, magic_resistance
+
+def create_entity(name, health, attack, physical_resistance, magic_resistance):
+    entity = Entity(name,
+                    health,
+                    attack,
+                    physical_resistance,
+                    magic_resistance)
+    return entity
--- test_entity.py
+++ test_entity.py
@@ -41,10 +41,38 @@ class TestEntity(unittest.TestCase):
     # test attribute magical_resistance
     def test_get_magic_resistance(self):
         self.assertEqual(self.entity.get_magic_resistance(), 0)
-
+    
     def test_set_magic_resistance(self):
         self.entity.set_magic_resistance(1)
         self.assertEqual(self.entity.get_magic_resistance(), 1)
+    
+    # test parsing input
+    def test_parse_input(self):
+        # create wrong data to parse
+        name_wrong = 1
+        health_wrong = "test"
+        attack_wrong = "test"
+        physical_resistance_wrong = "test"
+        magic_resistance_wrong = "test"
+        
+        # test wrong data
+        with self.assertRaises(ValueError):
+            entity.parse_input(name_wrong, health_wrong, attack_wrong, physical_resistance_wrong, magic_resistance_wrong)
+
+        # create correct data
+        name_correct = "test"
+        health_correct = "1"  
+        attack_correct = "1" 
+        physical_resistance_correct = "1" 
+        magic_resistance_correct = "1" 
+        
+        # test correct data
+        name, health, attack, physical_resistance, magic_resistance = entity.parse_input(name_correct, health_correct, attack_correct, physical_resistance_correct, magic_resistance_correct)
+        self.assertEqual(type(name), str)
+        self.assertEqual(type(health), float)
+        self.assertEqual(type(attack), float)
+        self.assertEqual(type(physical_resistance), float)
+        self.assertEqual(type(magic_resistance), float)
 
 if __name__ == '__main__':
     unittest.main()
